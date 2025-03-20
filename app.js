1b18bd7a70ff861be8ea3b17c9cad1e3cf0d12af
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-03-20 10:11:32 +0530

    uppercase and lowercase

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // strings methods 
 // format = stringName.method();
 
-let str = "     shubhi     "
-console.log(str.trim());
+let str = "shubhi"
+console.log(str.trim()); // creates new string
+// strings are immutable in JS
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
