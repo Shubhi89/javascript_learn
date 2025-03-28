1348279bb3134f21e38071d833bb71f5a1770eb9
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-03-28 09:59:25 +0530

    this keyword

--- app.js
+++ app.js
@@ -190,3 +190,15 @@ let greet = function() {
 }
 
 multipleGreet(greet , 4);
+
+// this keyword
+const studentInfo = {
+    age : 23,
+    eng:95,
+    math:93,
+    phy:97,
+    getavg() {
+        let avg = (this.eng+ this.math+this.phy)/3;
+        console.log(avg);
+    }
+};
