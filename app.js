a97d6ee7846957f3de3f9c5292d749ab33023c4f
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-04-02 15:13:57 +0530

    adding new para

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 let h4 = document.querySelector("h4");
 console.log(h4.parentElement);
+
+// adding new paragraph
+let newP = document.createElement('p');
+newP.innerText = "hello , this is a new paragraph";
+let body = document.querySelector("body");
+body.appendChild(newP);
