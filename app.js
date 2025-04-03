1d0891a11267adfc6d01bcf8fe810cd3c204c55a
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-04-03 09:58:32 +0530

    inline onclick

--- app.js
+++
@@ -1,8 +0,0 @@
-let h4 = document.querySelector("h4");
-console.log(h4.parentElement);
-
-// adding new paragraph
-let newP = document.createElement('p');
-newP.innerText = "hello , this is a new paragraph";
-let body = document.querySelector("body");
-body.appendChild(newP);
