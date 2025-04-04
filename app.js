fbfffacbdb061fd22ada028aaeddb0b5aec2f6e8
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-04-04 19:20:19 +0530

    extracting form info

--- app.js
+++ app.js
@@ -13,3 +13,12 @@ let btn2 = document.getElementById("btn2");
 btn2.addEventListener("click" , function() {
     console.log("button 2 is clicked");
 });
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit" , function(event) {
+    event.preventDefault();
+
+    let inp = document.querySelector("input");
+    console.log(inp.value);
+})
