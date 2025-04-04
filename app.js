c8c13718161f0bcdc02bb9467b4acc385f3c5bb4
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-04-04 09:29:38 +0530

    event listener

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let btn = document.querySelector("button");
+let btn = document.getElementById("btn1");
 console.dir(btn);
 
 btn.onclick = function() {
@@ -7,3 +7,9 @@ btn.onclick = function() {
 btn.onmouseenter = function() {
     console.log("you entered in button");
 }
+
+let btn2 = document.getElementById("btn2");
+
+btn2.addEventListener("click" , function() {
+    console.log("button 2 is clicked");
+});
