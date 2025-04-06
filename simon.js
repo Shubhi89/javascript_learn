cae19a9610fad13f592bbb495efcdec3c172c93c
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-04-06 21:33:47 +0530

    level up

--- simon.js
+++ simon.js
@@ -4,9 +4,24 @@ let userSeq = [];
 let started = false;
 let level = 0;
 
+let h2 = document.querySelector("h2");
+
 document.addEventListener("keypress" , function() {
     if(started == false) {
         console.log("game started");
         started = true;
+
+        levelUp();
     }
 });
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+}
+
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    btnFlash();
+}
