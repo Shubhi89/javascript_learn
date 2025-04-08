97e770ef512ea28dc758d75891eab5f7ff7e63a7
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-04-08 09:56:03 +0530

    promise syntax

--- app.js
+++ app.js
@@ -22,3 +22,15 @@ form.addEventListener("submit" , function(event) {
     let inp = document.querySelector("input");
     console.log(inp.value);
 })
+
+// promises
+function saveToDb(data) {
+    return new Promise((success , failure) => {
+        let internetSpeed = Math.floor(Math.random()*10)+1;
+    if(internetSpeed > 4) {
+        success("data saved");
+    } else {
+        failure("weak connection");
+    }
+    });
+}
