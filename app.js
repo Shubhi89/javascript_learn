92dfc744858f04ebc484067532fa095b82d3935d
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-04-10 12:01:28 +0530

    api call

--- app.js
+++ app.js
@@ -80,3 +80,16 @@ async function demo() {
     await getNum();
     await getNum();
 }
+
+// api calls
+let url = "https://catfact.ninja/fact";
+fetch(url)
+.then((res) => {
+    console.log(res);
+    res.json().then((data)=> {
+        console.log(data);
+    });
+})
+.catch((err) => {
+    console.log(err);
+});
